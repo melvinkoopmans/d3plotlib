99d47cbaf640ee0427485c845fc4650eecab7225
Melvin Koopmans <melvin.koopmans@gmail.com>
2020-05-01 23:04:16 +0200

    make network draggable

--- src/Network.ts
+++ src/Network.ts
@@ -42,37 +42,71 @@ class Network {
     }
 
     plot(network: NetworkStruct) {
-        const { svg } = this;
+        const { svg, width, height } = this;
         const scaleC = d3.scaleOrdinal<string>(d3.schemePastel1);
 
         d3.shuffle(network.nodes);
         d3.shuffle(network.edges);
 
-        d3.forceSimulation<Node, Edge>(network.nodes)
-            .force('ct', d3.forceCenter(300, 300))
-            .force('ln', d3.forceLink<Node, Edge>(network.edges).distance(40).id((d: Node) => d.id))
-            .force('hc', d3.forceCollide(10))
-            .force('many', d3.forceManyBody())
-            .on('end', function () {
-                svg.selectAll('line').data<SimulationEdge>(network.edges as SimulationEdge[]).enter()
-                    .append('line').attr('stroke', '#000')
-                    .attr('x1', (d) => d.source.x!)
-                    .attr('y1', (d) => d.source.y!)
-                    .attr('x2', (d) => d.target.x!)
-                    .attr('y2', (d) => d.target.y!);
+        const simulation = d3.forceSimulation<Node, Edge>(network.nodes)
+            .force('link', d3.forceLink<Node, Edge>(network.edges).id((d) => d.id))
+            .force('charge', d3.forceManyBody())
+            .force('center', d3.forceCenter(width / 2, height / 2));
 
-                svg.selectAll('circle').data(network.nodes).enter()
-                    .append('circle')
-                    .attr('r', 10).attr('fill', (d, i) => scaleC(i as any))
-                    .attr('cx', (d) => d.x!).attr('cy', (d) => d.y!);
+        const edges = svg.selectAll('line').data<SimulationEdge>(network.edges as SimulationEdge[]).enter()
+            .append('line').attr('stroke', '#000')
+            .attr('x1', (d) => d.source.x!)
+            .attr('y1', (d) => d.source.y!)
+            .attr('x2', (d) => d.target.x!)
+            .attr('y2', (d) => d.target.y!);
 
-                svg.selectAll('text').data(network.nodes).enter()
-                    .append('text')
-                    .attr('x', (d) => d.x!).attr('y', (d) => d.y! + 4)
-                    .attr('text-anchor', 'middle')
-                    .attr('font-size', 10)
-                    .text((d) => d.id);
+        const nodes = svg.selectAll('circle').data(network.nodes).enter()
+            .append('circle')
+            .attr('r', 10).attr('fill', (d, i) => scaleC(i as any))
+            .attr('cx', (d) => d.x!).attr('cy', (d) => d.y!)
+            .call(this.drag(simulation));
+
+        const labels = svg.selectAll('text').data(network.nodes).enter()
+            .append('text')
+            .attr('text-anchor', 'middle')
+            .attr('font-size', 10)
+            .text((d) => d.id);
+
+        simulation.on('tick', () => {
+            edges
+                .attr('x1', (d: SimulationEdge) => d.source.x!)
+                .attr('y1', (d: SimulationEdge) => d.source.y!)
+                .attr('x2', (d: SimulationEdge) => d.target.x!)
+                .attr('y2', (d: SimulationEdge) => d.target.y!);
+        
+            nodes
+                .attr('cx', (d) => d.x!)
+                .attr('cy', (d) => d.y!);
+
+            labels.attr('x', (d) => d.x!).attr('y', (d) => d.y! + 4)
+        });
+    }
+
+    drag(simulation: d3.Simulation<Node, Edge>) {
+        return d3.drag<SVGCircleElement, Node>()
+            .on('start', (d: Node) => {
+                if (!d3.event.active) {
+                    simulation.alphaTarget(0.3).restart();
+                }
+                d.fx = d.x;
+                d.fy = d.y;
             })
+            .on('drag', (d: Node) => {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            })
+            .on('end', (d: Node) => {
+                if (!d3.event.active) {
+                    simulation.alphaTarget(0);
+                }
+                d.fx = null;
+                d.fy = null;
+            });
     }
 } 
 
